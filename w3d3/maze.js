4fe2f9dbc8b5cb6516429cd2d1f28081fc766f4d
Luan Tran <luantran891@gmail.com>
2022-10-19 14:18:24 -0500

    Commit w3d3 - v1

---
+++ w3d3/maze.js
@@ -0,0 +1,65 @@
+
+$(function () {
+  const mazeEl = $("#maze");
+  const boundaryEls = $("#maze .boundary");
+  const startBtn = $("#start");
+  const endBtn = $("#end");
+  const status = $("#status");
+
+  let isStarted = false;
+  let isLost = false;
+
+  mazeEl.mouseover(function(e) {
+    const $e = $(e.target);
+    if ($e.hasClass("boundary")) {
+      boundaryEls.addClass("youlose");
+      if (isStarted) {
+        lost();
+      }
+    }
+  });
+
+  mazeEl.mouseleave(function(e) {
+    if (isStarted) {
+      boundaryEls.addClass("youlose");
+      lost();
+    }
+  });
+
+  boundaryEls.mouseleave(function(e) {
+    const $e = $(e.target);
+    if ($e.hasClass("youlose") && !isLost) {
+      boundaryEls.removeClass("youlose");
+    }
+  });
+
+  endBtn.mouseenter(function(e) {
+    if (isStarted) {
+      win();
+    }
+  });
+
+  startBtn.click(function() {
+    reset();
+  });
+
+  function reset() {
+    status.text("Click the \"S\" to begin.");
+    isStarted = !isStarted;
+    isLost = false;
+    boundaryEls.removeClass("youlose");
+  }
+
+  function win() {
+    // alert("You win! :]");
+    status.text("You win! :]");
+    isStarted = !isStarted;
+  }
+
+  function lost() {
+    // setTimeout(() => {alert("Sorry, you lost! :[")}, 100);
+    status.text("Sorry, you lost! :[");
+    isStarted = !isStarted;
+    isLost = true;
+  }
+});
